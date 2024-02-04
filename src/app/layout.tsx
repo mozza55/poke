130dd3aee73c2bb70a7341807f627844b110f262
mozza55 <mozzarella1103@gmail.com>
2024-02-04 14:43:10 +0900

    feat: provider 컴포넌트 추가
    - react-query 프로바이더 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import Providers from './provider';
 
 export const metadata: Metadata = {
   title: '포켓몬 도감',
@@ -9,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body className="h-full w-screen overflow-x-hidden bg-white">
-        <main className="w-screen">{children}</main>
+        <Providers>
+          <main className="w-screen">{children}</main>
+        </Providers>
       </body>
     </html>
   );
---
+++ src/app/provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
